621a431ff158e26767babf08b8afaf3c3fe6d0fd
hartbold <hartbold8@gmail.com>
2022-12-17 22:08:22 +0100

    initial refractor and button to comunicate with the API

---
+++ src/js/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+
+import './../css/App.css';
+import React from 'react';
+import UploadButton from './components/UploadButton';
+
+function App() {
+  return (
+    <div>
+      <UploadButton/>
+    </div>
+  );
+} 
+
+export default App;
